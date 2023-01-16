e6b1899788e1eb6d4409a05cc30202906a04b9cd
HuishanWen <122833807+HuishanWen@users.noreply.github.com>
2023-01-16 16:25:34 -0500

    Add files via upload

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,106 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+
+import "./cart.css";
+export const Cart = () => {
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/checkout");
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
+    </div>
+  );
+};
+
+
+
+
+
+
+/*import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+
+
+import "./cart.css";
+export const Cart = () => {
+    const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+    const totalAmount = getTotalCartAmount();
+    
+    const navigate = useNavigate();
+    
+    return (
+        <div className="cart">
+        <div>
+            <h1> Your cart Items </h1>
+        </div>
+        <div className="cart">
+            {PRODUCTS.map((product)=>{
+                if(cartItems[product.id] !== 0){
+                    return <CartItem data={product} />;
+                }
+                
+            }
+           
+            )}
+        </div>
+            
+            {totalAmount > 0 ?(
+            <div className="checkout">
+                
+                <p> Subtotal: ${totalAmount}</p>
+                <button onClick={() => navigate("/")}> Continue Shopping </button>
+                <button 
+                    onClick={()=>{
+                            checkout();
+                            navigate("/checkout");
+                        }
+                        }
+                    >
+                    {" "}
+            Checkout{" "}
+                            </button>
+               
+            </div>
+                ) : (
+             
+                <h1>Your Cart is Empty</h1>
+                )}
+    </div>
+        );
+};*/
